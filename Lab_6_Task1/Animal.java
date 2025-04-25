583c771283b0bad640cbd68debcf102bc03811f6
Ali Usama <aliusama.king@gmail.com>
2025-04-25 07:58:56 +0500

    Add files via upload

---
+++ Lab_6_Task1/Animal.java
@@ -0,0 +1,8 @@
+class Animal {
+    String name;
+    int age;
+
+    public void makeSound() {
+        System.out.println("Animal Sound");
+    }
+}
---
+++ Lab_6_Task1/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog d1 = new Dog();
+        d1.makeSound(); 
+    }
+}
