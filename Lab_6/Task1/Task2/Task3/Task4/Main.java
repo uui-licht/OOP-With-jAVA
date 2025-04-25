15b2419dcaf195dc4479507541d53508a6b42204
Ali Usama <aliusama.king@gmail.com>
2025-04-25 07:44:42 +0500

    Add files via upload

---
+++ Lab_6/Task1/Task2/Task3/Task4/Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) {
+        Shape shape;
+        
+        shape = new Circle();
+        shape.draw();
+        
+        shape = new Rectangle();
+        shape.draw();
+    }
+}
